df14a6b6a5c25463467dec940360798c3c2d80fa
Lee Dogeon <dev.moreal@gmail.com>
2021-01-27 01:49:42 +0900

    feat: implement rocksdb command
    - rocksdb get
    - rocksdb set

--- RocksDBTool/RocksDbCommand.cs
+++ RocksDBTool/RocksDbCommand.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using Cocona;
+using RocksDbSharp;
 
 namespace RocksDBTool
 {
@@ -17,12 +18,73 @@ namespace RocksDBTool
 
         public int Get([Argument] InputOutputFormat inputOutputFormat, [Argument] string key)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using var db = _rocksDbService.Load();
+                switch (inputOutputFormat)
+                {
+                    case InputOutputFormat.Base64:
+                        if (db.Get(Convert.FromBase64String(key)) is {} bytesValue)
+                        {
+                            _output.Write(Convert.ToBase64String(bytesValue));   
+                            return 0;
+                        }
+                        break;
+
+                    case InputOutputFormat.String:
+                        if (db.Get(key) is {} stringValue)
+                        {
+                            _output.Write(stringValue);   
+                            return 0;
+                        }
+                        break;
+                }
+            }
+            catch (RocksDbException e)
+            {
+                // FIXME: should be written into stderr instead of stdout.
+                _output.WriteLine($"{nameof(RocksDbException)} occurred.");
+                _output.WriteLine(e.Message);
+            }
+            catch (ArgumentNullException e)
+            {
+                // FIXME: should be written into stderr instead of stdout.
+                _output.WriteLine("The configuration seems not configured yet. you ");
+            }
+
+            return -1;
         }
 
         public int Set([Argument] InputOutputFormat inputOutputFormat, [Argument] string key, [Argument] string value)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using var db = _rocksDbService.Load();
+                switch (inputOutputFormat)
+                {
+                    case InputOutputFormat.Base64:
+                        db.Put(Convert.FromBase64String(key), Convert.FromBase64String(value));
+                        break;
+                    case InputOutputFormat.String:
+                        db.Put(key, value);
+                        break;
+                }
+
+                return 0;
+            }
+            catch (RocksDbException e)
+            {
+                // FIXME: should be written into stderr instead of stdout.
+                _output.WriteLine($"{nameof(RocksDbException)} occurred.");
+                _output.WriteLine(e.Message);
+            }
+            catch (ArgumentNullException e)
+            {
+                // FIXME: should be written into stderr instead of stdout.
+                _output.WriteLine("The configuration seems not configured yet. you ");
+            }
+
+            return -1;
         }
     }
 }
