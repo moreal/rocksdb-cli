6b36fb221635e96df89fdca2cfae68087613df91
Lee Dogeon <dev.moreal@gmail.com>
2021-01-27 02:19:08 +0900

    feat: run application in command line

--- RocksDBTool/Program.cs
+++ RocksDBTool/Program.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿#nullable enable
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Cocona;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace RocksDBTool
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            await CoconaApp.Create().ConfigureServices(
+                services =>
+                {
+                    services.AddTransient<IRocksDbService, RocksDbService>();
+                    services.AddTransient<TextWriter>(serviceProvider => Console.Out);
+                }).ConfigureAppConfiguration(
+                (context, builder) =>
+                {
+                    var path = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                        ".config",
+                        "rocksdb-tool.json");
+                    builder.AddJsonFile(path, true);
+                }).RunAsync<RocksDbCommand>(args);
         }
     }
 }
